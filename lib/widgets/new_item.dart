a35a5d26eb4cb309d359b2d901ff4df601205ab5
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-11-24 20:35:29 +0530

    Adding Form-aware DropDown Button

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app/data/categories.dart';
 
 class NewItem extends StatelessWidget {
   const NewItem({super.key});
@@ -11,17 +12,57 @@ class NewItem extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(12),
-        child: Form(child: Column(children: [
-          TextFormField(
-            maxLength: 50,
-            decoration: InputDecoration(
-              label: Text('Name'),
-            ),
-            validator: (value){
-              return '...';
-            },
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Name'),
+                ),
+                validator: (value) {
+                  return '...';
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(label: Text('Quantity')),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        //~ 'categories' is a map which cannot be used in a list
+                        //~ So we convert it to list using special entries property that is available on all dart maps
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.categoryColor,
+                                ),
+                                const SizedBox(width: 6),
+                                Text(category.value.title),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ],),),
+        ),
       ),
     );
   }
